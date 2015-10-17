bb4c7a2ddeb44bcafc7cd2976fae0c1abd805715
S <s>
2015-10-18 01:49:52 +0200

    {"type":"TI", "runs":"0"}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -8,7 +8,7 @@
  */
 import stanford.karel.*;
 
-public class CollectNewspaperKarel extends karelMejorada {
+public class CollectNewspaperKarel extends KarelMejorada {
 
 		public void run(){ 
 		while (frontIsClear()){
