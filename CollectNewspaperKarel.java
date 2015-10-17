13628e3dcb84bf53276c994b132520a5c182fb6a
S <s>
2015-10-18 00:57:05 +0200

    {"type":"TI", "runs":"9"}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -1,16 +1,14 @@
 /*
- * File: CollectNewspaperKarel.java
- * --------------------------------
- * At present, the CollectNewspaperKarel subclass does nothing.
- * Your job in the assignment is to add the necessary code to
- * instruct Karel to walk to the door of its house, pick up the
- * newspaper (represented by a beeper, of course), and then return
- * to its initial position in the upper left corner of the house.
+ * 				Autor: Jorge García Rojo
+ * 
+ * Conseguir que Karel salga de la "casa" por el hueco,
+ * coger el "periódico" (beeper), volver a casa y
+ * dejar el "periódico" (beeper) en el lugar de salida de Karel.
+ *
  */
-
 import stanford.karel.*;
 
-public class CollectNewspaperKarel extends Karel {
+public class CollectNewspaperKarel extends KarelMejorada {
 	
 	// You fill in this part
 
