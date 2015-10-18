f8e6c15027974dd3ad164f5f882724651a427ab0
S <s>
2015-10-18 23:02:26 +0200

    {"type":"TI", "runs":"1"}

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,4 +1,20 @@
+import stanford.karel.*;
 
-public class Ejercicio3 {
 
+public class Ejercicio3 extends KarelMejorada {
+
+	public void run() {
+		
+	}
+	private void recogeTodosLosBeepers(){
+		while (frontIsClear()){
+			limpiaUnaColumna();
+			move();
+		}
+	}
+	private void limpiaUnaColumna(){
+		turnLeft();
+		recogeBeepersUnaColumna();
+		daLaVuelta();
+	}
 }
