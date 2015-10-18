83da4071ed1cc9f684724576c88f4e7093ff24ad
S <s>
2015-10-18 12:51:21 +0200

    {"type":"TI", "runs":"0"}

--- CollectNewspaperKarelGigante.java
+++ CollectNewspaperKarelGigante.java
@@ -1,4 +1,41 @@
+/*
+ * 				Autor: Jorge García Rojo
+ * 
+ * Conseguir que Karel salga de la "casa" por el hueco,
+ * coger el "periódico" (beeper), volver a casa y
+ * dejar el "periódico" (beeper) en el lugar de salida de Karel.
+ *
+ */
+import stanford.karel.*;
 
-public class CollectNewspaperKarelGigante {
+public class CollectNewspaperKarelGigante extends KarelMejorada {
+
+	public void run(){
+
+		// moverHastaPared
+		// girar a la derecha
+		// si no hay pared a la izquierda girar a la izquierda
+		// cuando haya un beeper cogerlo y dar la vuelta
+
+
+		while (frontIsClear()){
+			while (frontIsClear()){
+				move();				
+				if (leftIsClear()){
+					turnLeft();
+				}
+				if (beepersPresent()){
+					pickBeeper();
+					retornaAlInicio();
+					putBeeper();
+
+				}
+
+			}
+			giraDerecha();
+
+		}	
+		giraDerecha();
+	}
 
 }
