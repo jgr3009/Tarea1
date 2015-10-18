f4862008ebda17f0db09030adee9dfce954c502a
S <s>
2015-10-18 18:00:55 +0200

    {"type":"TI", "runs":"1"}

--- CollectNewspaperKarelGigante.java
+++ CollectNewspaperKarelGigante.java
@@ -19,7 +19,7 @@ public class CollectNewspaperKarelGigante extends KarelMejorada {
 
 
 		while (frontIsClear()){
-			while (frontIsClear()){
+			if (frontIsClear()){
 				move();				
 				if (leftIsClear()){
 					turnLeft();
