4048a4da54444f1916d1cbbb0bc80a073f3b28c9
S <s>
2015-10-18 13:27:06 +0200

    {"type":"TI", "runs":"1"}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -21,7 +21,7 @@ public class CollectNewspaperKarel extends KarelMejorada {
 		while (frontIsClear()){
 			while (frontIsClear()){
 				move();				
-				
+				{
 			
 			giraDerecha();
 
@@ -32,6 +32,6 @@ public class CollectNewspaperKarel extends KarelMejorada {
 				putBeeper();
 			
 		}
-			}
+			}}
 	}
 }
