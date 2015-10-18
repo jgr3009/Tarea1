3a95633f61c390b96744bfaed2fd14bfcd63370d
S <s>
2015-10-18 19:12:38 +0200

    {"type":"TI", "runs":"1"}

--- CollectNewspaperKarelGigante.java
+++ CollectNewspaperKarelGigante.java
@@ -27,7 +27,7 @@ public class CollectNewspaperKarelGigante extends KarelMejorada {
 						if (beepersPresent()){
 							pickBeeper();
 							retornaAlInicio();
-							putBeeper();
+							
 						}
 					}
 				}
