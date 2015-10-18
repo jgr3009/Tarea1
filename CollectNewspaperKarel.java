dbf725e0ca3797473260b0c9081d75ce3c3dde32
S <s>
2015-10-18 18:59:15 +0200

    {"type":"TI", "runs":"1"}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -29,7 +29,7 @@ public class CollectNewspaperKarel extends KarelMejorada {
 						if (beepersPresent()){
 							pickBeeper();
 							retornaAlInicio();
-							putBeeper();
+							
 						}
 					}
 				}
@@ -39,7 +39,7 @@ public class CollectNewspaperKarel extends KarelMejorada {
 			giraDerecha();
 
 		}
-		
+		putBeeper();
 	}
 
 }
