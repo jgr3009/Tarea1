68afc99ef2a85ec6a1341eeffb9d3f1c0ecbdf21
S <s>
2015-10-18 11:02:58 +0200

    {"type":"TI", "runs":"2"}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -25,6 +25,9 @@ public class CollectNewspaperKarel extends KarelMejorada {
 				if (leftIsClear()){
 					turnLeft();
 				}			
+				if (beepersPresent()){
+					pickBeeper();
+				}
 			}
 			giraDerecha();
 			
