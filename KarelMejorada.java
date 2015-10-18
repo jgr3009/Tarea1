5b4bb2997da88df1cb2c316c34a9b0e0cdeeff5e
S <s>
2015-10-18 19:43:13 +0200

    {"type":"TI", "runs":"2"}

--- KarelMejorada.java
+++ KarelMejorada.java
@@ -51,18 +51,18 @@ public class KarelMejorada extends Karel{
 	}
 	public void retornaAlInicio(){
 		dalaVuelta();
-		
+		while (frontIsClear()){
+			move();
 			if (frontIsBlocked()){
 				giraDerecha();
 			}
 			if (facingNorth()){
 				move();
-				putBeeper();
-				turnLeft();
+				
 			}
 		
-			
-		
+		}
+		turnLeft();
 	}
 
 }
