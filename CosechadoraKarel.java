f7e312f59f47fed8fe67f66418b34a31e0552603
S <s>
2015-10-20 23:47:41 +0200

    {"type":"TI", "runs":"1"}

--- CosechadoraKarel.java
+++ CosechadoraKarel.java
@@ -48,11 +48,10 @@ public class CosechadoraKarel extends KarelMejorada{
 	}
 	private void limpiaUnaFila(){
 		//Karel se mueve por toda la fila y recoge todos los beepers que haya
-		while (frontIsClear()){
-			move();
+		
 			if (beepersPresent()){
 				pickBeeper();
-			}
+			
 		}
 	}
 	private void subeUnaFilaParaIzquierda(){
