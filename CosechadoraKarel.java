f81cbc303859cf01a3cda7cef4e097dd7f96198d
S <s>
2015-10-21 23:04:02 +0200

    {"type":"TI", "runs":"1"}

--- CosechadoraKarel.java
+++ CosechadoraKarel.java
@@ -30,9 +30,9 @@ public class CosechadoraKarel extends KarelMejorada{
 				subeUnaFilaParaDerecha();
 				
 			}
-			
+			retornaAlInicio();
 		}
-		retornaAlInicio();
+		
 	}
 	private void limpiaUnaFila(){
 		//Karel se mueve por toda la fila y recoge todos los beepers que haya
