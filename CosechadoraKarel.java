acc131cb499821a575733e1fb3813614e9bf6759
S <s>
2015-10-21 23:01:59 +0200

    {"type":"TI", "runs":"1"}

--- CosechadoraKarel.java
+++ CosechadoraKarel.java
@@ -28,9 +28,9 @@ public class CosechadoraKarel extends KarelMejorada{
 			if (facingWest()||frontIsBlocked()){
 				limpiaUnaFila();
 				subeUnaFilaParaDerecha();
-				retornaAlInicio();
+				
 			}
-
+			retornaAlInicio();
 		}
 		
 	}
