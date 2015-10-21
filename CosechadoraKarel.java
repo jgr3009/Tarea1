2a7676c016763cccaf4df1888617b9216a9f075b
S <s>
2015-10-21 23:13:35 +0200

    {"type":"TI", "runs":"1"}

--- CosechadoraKarel.java
+++ CosechadoraKarel.java
@@ -48,7 +48,7 @@ public class CosechadoraKarel extends KarelMejorada{
 	private void subeUnaFilaParaDerecha(){
 		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
 		if (rightIsBlocked()){
-			daLaVuelta();
+			turnLeft();
 			while (frontIsClear()){
 			move();
 			}
