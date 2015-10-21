e47d4624c43cd1786258375f14c0067dcc651e99
S <s>
2015-10-21 22:33:54 +0200

    {"type":"TI", "runs":"2"}

--- CosechadoraKarel.java
+++ CosechadoraKarel.java
@@ -69,7 +69,7 @@ public class CosechadoraKarel extends KarelMejorada{
 		limpiaColumnaDeSubida();
 		limpiaColumnaDeBajada();
 
-		//turnLeft();
+		turnLeft();
 		while(frontIsClear()){
 			move();
 		}
