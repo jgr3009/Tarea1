3a13c276e9fb00cd4fd08d73e93fc2136199e091
S <s>
2015-10-25 13:35:02 +0100

    {"type":"TI", "runs":"1"}

--- MudanzasKarel.java
+++ MudanzasKarel.java
@@ -7,10 +7,11 @@ public class MudanzasKarel extends KarelMejorada{
 	public void run(){
 
 		while (frontIsClear()){
-			move();
+			
 			if (beepersPresent()){
 				pickBeeper();
 			}
+			move();
 		}
 		
 
