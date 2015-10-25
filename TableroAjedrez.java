9ec7ebdde682ce35636dde8c94605a30b95e5927
S <s>
2015-10-25 10:47:23 +0100

    {"type":"TI", "runs":"0"}

--- TableroAjedrez.java
+++ TableroAjedrez.java
@@ -7,59 +7,10 @@ public class TableroAjedrez extends KarelMejorada{
 
 	public void run(){
 
-		//subir a Karel a la segunda fila
-
-		//limpia todos los beepers de una fila
-
-		//Cuando llegue al final de la fila sube a la de arriba
-
-
-		ponerBeeper();
-	}
-
-
-	private void ponerBeeper(){
-		//Karel se mueve por toda la fila y recoge todos los beepers que haya
 		
-			if (facingEast()||frontIsBlocked()){
-
-				subeUnaFilaParaIzquierda();
-
-
-			}
-			if (facingWest()||frontIsBlocked()){
-
-				subeUnaFilaParaDerecha();
-
-			}
-			
-
 		
-		move();
-		putBeeper();
-		move();
+		
+		
+		
+		
 	}
-	private void subeUnaFilaParaDerecha(){
-		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
-
-		turnRight();
-		move();
-		turnRight();
-		/*if (rightIsBlocked()){
-				turnLeft();
-				while (frontIsClear()){
-				move();
-
-				}*/	
-	}
-
-
-	private void subeUnaFilaParaIzquierda(){
-		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
-		turnLeft();
-		move();
-		turnLeft();
-
-	}
-
-}
