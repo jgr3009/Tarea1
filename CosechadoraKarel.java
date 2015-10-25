c12f132bb5f701a9cbf358ac0ae5510d1499615e
S <s>
2015-10-25 11:47:20 +0100

    {"type":"TI", "runs":"2"}

--- CosechadoraKarel.java
+++ CosechadoraKarel.java
@@ -53,7 +53,7 @@ public class CosechadoraKarel extends KarelMejorada{
 		if (frontIsBlocked()){
 			daLaVuelta();}
 		move();
-		while (frontIsClear()){
+		while (frontIsClear()&&facingSouth()){
 			move();
 			if (frontIsBlocked()&&facingSouth()){
 				daLaVuelta();
