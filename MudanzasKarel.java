83ec5346d733a2fbb6471faac5bc4084400548d0
S <s>
2015-10-26 21:20:04 +0100

    {"type":"TI", "runs":"1"}

--- MudanzasKarel.java
+++ MudanzasKarel.java
@@ -26,21 +26,27 @@ public class MudanzasKarel extends KarelMejorada{
 			}
 			for (int i=	0;i	<6;i++)	{
 				putBeeper();
+				move();
 			}
 			for (int i=	0;i	<3;i++)	{
 				putBeeper();
+				move();
 			}
 			for (int i=	0;i	<2;i++)	{
 				putBeeper();
+				move();
 			}
 			for (int i=	0;i	<8;i++)	{
 				putBeeper();
+				move();
 			}
 			for (int i=	0;i	<1;i++)	{
 				putBeeper();
+				move();
 			}
 			for (int i=	0;i	<16;i++)	{
 				putBeeper();
+				move();
 			}
 		}
 
