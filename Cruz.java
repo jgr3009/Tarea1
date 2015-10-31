883e6b71a90565ef141131223ac7f1144406b7b9
S <s>
2015-11-01 00:00:50 +0100

    {"type":"TI", "runs":"2"}

--- Cruz.java
+++ Cruz.java
@@ -5,5 +5,53 @@ public class Cruz extends KarelMejorada{
 
 	public void run(){
 		
+		while (frontIsClear()||facingSouth()){
+
+			if (facingEast()||frontIsBlocked()){
+				encuentraBeeper();
+				subeUnaFilaParaIzquierda();
+			}
+			if (facingWest()||frontIsBlocked()){
+				encuentraBeeper();
+				subeUnaFilaParaDerecha();
+			}
+		}
+		daLaVuelta();
 	}
+
+
+	private void encuentraBeeper(){
+		//Karel se mueve por toda la fila y recoge todos los beepers que haya
+		while (frontIsClear()){
+			move();
+			if (beepersPresent()){
+				pickBeeper();
+			}
+		}
+	}
+	private void subeUnaFilaParaDerecha(){
+		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
+
+		turnRight();
+		if (frontIsBlocked()&&facingNorth()){
+			daLaVuelta();}
+		move();
+		while (frontIsClear()&&facingSouth()){
+			move();
+		}
+		turnRight();
+	}
+
+
+	private void subeUnaFilaParaIzquierda(){
+		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
+		turnLeft();
+		move();
+		turnLeft();
+
+	}
+
+
+	
+	
 }
