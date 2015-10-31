13ef040164ef0ade9a9973e443139508578c78e1
S <s>
2015-10-31 23:28:20 +0100

    {"type":"TI", "runs":"4"}

--- SaltaAgujas.java
+++ SaltaAgujas.java
@@ -1,8 +1,45 @@
+/*
+ * Autor: JORGE GARCÍA ROJO
+ * 
+ * En este ejercicio Karel empiez en la esquina inferior izquierda 
+ * y mierando al este. Tiene que saltar las agujas, de una en una, sin soltar la aguja que está saltando.
+ * Cuando llegue al final, debe subir hasta arriba y volver a bajar.
+ * 
+ */
+
+
 import stanford.karel.*;
 
 public class SaltaAgujas extends KarelMejorada{
 
 	public void run(){
+		saltaLasAgujas();
+		/*while (frontIsBlocked()){
+			turnLeft();
+			while (frontIsClear()){
+				move();
+				if (rightIsClear()){
+					turnRight();
+					move();
+					turnRight();
+				}
+			}*/
+			subeUltimaColumna();
+			/*if (frontIsBlocked()&&facingNorth()){
+				daLaVuelta();
+			}*/
+		
+		llegaAlFinal();
+		/*while (frontIsClear()){
+			move();
+			if (frontIsBlocked()&&facingNorth()){
+				turnLeft();
+			}
+		}*/
+		turnLeft();
+
+	}
+	private void saltaLasAgujas(){
 		while (frontIsBlocked()){
 			turnLeft();
 			while (frontIsClear()){
@@ -13,17 +50,20 @@ public class SaltaAgujas extends KarelMejorada{
 					turnRight();
 				}
 			}
-			if (frontIsBlocked()&&facingNorth()){
-				daLaVuelta();
-			}
 		}
+	}
+	
+	private void subeUltimaColumna(){
+		if (frontIsBlocked()&&facingNorth()){
+			daLaVuelta();
+		}
+	}
+	private void llegaAlFinal(){
 		while (frontIsClear()){
 			move();
 			if (frontIsBlocked()&&facingNorth()){
 				turnLeft();
 			}
 		}
-		turnLeft();
 	}
-}
-
+	}
