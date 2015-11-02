e824571fd1ef3bfe0d2a68e9c5cb973fe2aaa030
S <s>
2015-11-02 21:33:58 +0100

    {"type":"TI", "runs":"0"}

--- MudanzasKarel.java
+++ MudanzasKarel.java
@@ -2,11 +2,12 @@
 /*
  * Autor: JORGE GARCÍA ROJO
  * 
- * En este ejercicio Karel está en un mundo de empieza en la esquina inferior izquierda y mirando al este, y
+ * En este ejercicio Karel está en un mundo de 19x7 y empieza en la esquina inferior izquierda y mirando al este, y
  * consiste en que Karel coja todos los beepers del lado 
- * izquierdo de la aguja en el siguiente orden (6,3,0,2,8,1,16), pase hacia 
+ * izquierdo de la aguja (situada entre 8 y 9) en el siguiente orden (6,3,0,2,8,1,16), pase hacia 
  * el otro lado de la aguja y vaya dejando los beepers en el siguiente orden (6,3,2,8,1,16). 
- * Y cuando deje el último beeper vuelva a su posición inicial (esquina inferior izquierda y mirando al este).
+ * Y cuando deje el último beeper vuelva a su posición inicial (esquina inferior izquierda y mirando al este),
+ * bordeando el mundo por el este, norte y oeste.
  */
 
 
