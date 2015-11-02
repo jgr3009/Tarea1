0b6b316dab130ec8dedea19a8be9d512f4860489
S <s>
2015-11-02 22:44:51 +0100

    {"type":"TI", "runs":"0"}

--- Cruz.java
+++ Cruz.java
@@ -2,7 +2,12 @@
 /*
  * Auto: JORGE GARCÍA ROJO
  * 
- * 
+ * En este ejercicio Karel empieza en la esquina inferior izquierda, mirando al este, con infinitos beepers
+ * y en un mundo de 10x10, y con un beeper en el 4x6. Karel tiene que buscar el beeper 
+ * y dibujar una linea de beeper desde el propio beeper hasta la pared de la derecha, volver y dibujar
+ * otra línea desde el beeper que había al inicio hasta el suelo. 
+ * Después de Karel debe volver al inicio quedándose en la esquina inferior izquierda y mirando al este.
+ *
  */
 
 import stanford.karel.*;
