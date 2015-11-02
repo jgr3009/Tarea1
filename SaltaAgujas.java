59934f44b75a025e6af812fce99e9ba3c9570d43
S <s>
2015-11-02 21:47:08 +0100

    {"type":"TI", "runs":"0"}

--- SaltaAgujas.java
+++ SaltaAgujas.java
@@ -3,7 +3,8 @@
  * 
  * En este ejercicio Karel empieza en la esquina inferior izquierda 
  * y mierando al este en un mundo de 9x10. Tiene que saltar las agujas, de una en una, sin soltar la aguja que está saltando.
- * Cuando llegue al final, debe subir hasta arriba, volver a bajar y quedarse mirando al este.
+ * Cuando salte la última aguja, debe subir hasta arriba, volver a bajar y quedarse en la esquina 
+ * inferior derecha mirando al este.
  * 
  */
 
