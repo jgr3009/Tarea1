724a180192d04ea8bfb5ecbd824a017cab7766a8
S <s>
2015-11-02 22:24:22 +0100

    {"type":"TI", "runs":"1"}

--- Cruz.java
+++ Cruz.java
@@ -6,86 +6,76 @@ public class Cruz extends KarelMejorada{
 	public void run(){
 		while (frontIsClear()){
 			move();
-		
+
 			subeUnaFilaParaIzquierda();
 			subeUnaFilaParaDerecha();
 			encuentraBeeper();
-			
-	
 		}
 		vuelveAlInicio();
-		
 	}
+
+
 	private void vuelveAlInicio(){
-		
+
 		if (frontIsBlocked()){
 			turnRight();
 			while (frontIsClear()){
-			move();
-			
+				move();
 			}
 			daLaVuelta();
-			}
+		}
 	}
+
 
 	private void subeUnaFilaParaDerecha(){
 		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
-		
+
 		if (facingWest()&&frontIsBlocked()){
 			turnRight();
 			move();
 			turnRight();
 		}
-		}
-	
+	}
+
 
 
 	private void subeUnaFilaParaIzquierda(){
 		//Hace que Karel suba una fila hacia su derecha cuando llega a la pared
-		
+
 		if (facingEast()&&frontIsBlocked()){
 			turnLeft();
 			move();
 			turnLeft();
-		
-	}
+		}
 	}
 	private void encuentraBeeper(){
 
 		//while (frontIsClear()){
 		//	move();
-			if (beepersPresent()){
-				pickBeeper();
+		if (beepersPresent()){
+			pickBeeper();
+			daLaVuelta();
+			while (frontIsClear()){
+				move();
+				putBeeper();
+			}
+			if (frontIsBlocked()){
 				daLaVuelta();
+				while (beepersPresent()){
+					move();
+				}
+				if (noBeepersPresent()){
+					putBeeper();
+					turnLeft();
+				}
 				while (frontIsClear()){
 					move();
 					putBeeper();
-				}
-				if (frontIsBlocked()){
-					daLaVuelta();
-					while (beepersPresent()){
-						move();
-					}
-					if (noBeepersPresent()){
-						putBeeper();
-						turnLeft();
-					}
-					while (frontIsClear()){
-						move();
-						putBeeper();
-						while (frontIsClear()&&facingWest()){
-							move();
-						}
-					}
-
+					//while (frontIsClear()&&facingWest()){
+						//move();
+					//}
 				}
 			}
-
-		//}
-
-
+		}
 	}
-
-
-
 }
