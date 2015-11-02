33d60de451ecef5071c4f3b7b732b82102b512a7
S <s>
2015-11-02 21:01:50 +0100

    {"type":"TI", "runs":"3"}

--- MudanzasKarel.java
+++ MudanzasKarel.java
@@ -7,60 +7,13 @@ public class MudanzasKarel extends KarelMejorada{
 	public void run(){
 
 		cogeLosBeeper();
-		
-		/*while (frontIsClear()){
-			move();
-			while (beepersPresent()){
-				pickBeeper();
-			}
-		}
-		turnLeft();
-		*/
 		saltaAguja();
-		
-		/*while (frontIsClear()){
-			move();
-
-			if (rightIsClear()){
-				turnRight();
-			}
-		}
-		*/
 		ponerTodosLosBeeper();
-		
 		vueltaAlInicio();
 	}
-		/*while (frontIsClear()){
-			move();
-			if (frontIsBlocked()&&notFacingSouth()){
-				turnLeft();
-			}
-		
-		}
-		turnLeft();
-	}
-	*/
-	private void vueltaAlInicio(){
-		while (frontIsClear()){
-			move();
-			if (frontIsBlocked()&&notFacingSouth()){
-				turnLeft();
-			}
-		
-		}
-		turnLeft();
-	}
-	
-	private void saltaAguja(){
-		while (frontIsClear()){
-			move();
 
-			if (rightIsClear()){
-				turnRight();
-			}
-		}
-	}
-	
+
+
 	private void cogeLosBeeper(){
 		while (frontIsClear()){
 			move();
@@ -71,42 +24,54 @@ public class MudanzasKarel extends KarelMejorada{
 		turnLeft();
 	}
 
-
-private void ponerTodosLosBeeper(){
-	for (int i=	0;i	<6;i++)	{
-		putBeeper();
-	}
-	turnLeft();
-	move();
-	for (int i=	0;i	<3;i++)	{
-		putBeeper();
-	}
-	move();
-	for (int i=	0;i	<2;i++)	{
-		putBeeper();
-	}
-	move();	
-	for (int i=	0;i	<8;i++)	{
-		putBeeper();
-	}
-	move();	
-	for (int i=	0;i	<1;i++)	{
-		putBeeper();
-	}
-	move();	
-	for (int i=	0;i	<16;i++) {
-		putBeeper();
-	}
-	move();
-}
-/*private void retornaAlInicio(){
+	private void saltaAguja(){
 		while (frontIsClear()){
 			move();
-			if (frontIsBlocked()){
-				turnLeft();
+
+			if (rightIsClear()){
+				turnRight();
 			}
 		}
+	}
 
-	}*/
+
+	private void ponerTodosLosBeeper(){
+		for (int i=	0;i	<6;i++)	{
+			putBeeper();
+		}
+		turnLeft();
+		move();
+		for (int i=	0;i	<3;i++)	{
+			putBeeper();
+		}
+		move();
+		for (int i=	0;i	<2;i++)	{
+			putBeeper();
+		}
+		move();	
+		for (int i=	0;i	<8;i++)	{
+			putBeeper();
+		}
+		move();	
+		for (int i=	0;i	<1;i++)	{
+			putBeeper();
+		}
+		move();	
+		for (int i=	0;i	<16;i++) {
+			putBeeper();
+		}
+		move();
+	}
+
+	private void vueltaAlInicio(){
+		while (frontIsClear()){
+			move();
+			if (frontIsBlocked()&&notFacingSouth()){
+				turnLeft();
+			}
+
+		}
+		turnLeft();
+	}
 
 }
