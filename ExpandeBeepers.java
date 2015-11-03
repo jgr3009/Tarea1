98954f893fdfb174cbd2ceef726d679acb79fa02
S <s>
2015-11-03 22:55:06 +0100

    {"type":"TI", "runs":"1"}

--- ExpandeBeepers.java
+++ ExpandeBeepers.java
@@ -31,10 +31,7 @@ public class ExpandeBeepers extends KarelMejorada{
 						move();
 					}
 					turnLeft();
-					if (beepersPresent()){
-						pickBeeper();
-					}
-				
+									
 				}
 			}
 		}
